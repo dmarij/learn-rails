1d35a82425cf4dd45c82367ad6cb74a235a5f8c2
dmarij <dmarijancic@gmail.com>
2013-09-13 20:36:27 +0000

    dynamic home page

---
+++ app/models/owner.rb
@@ -0,0 +1,13 @@
+class Owner
+  def name
+    name = "Foobar Kardigan"
+  end
+  def birthdate
+    birthdate = Date.new(1990, 9, 22)
+  end
+  def countdown
+    today = Date.today
+    birthday = Date.new(today.year, birthdate.month, birthdate.day)
+    countdown = (birthday - today).to_i
+  end
+end
